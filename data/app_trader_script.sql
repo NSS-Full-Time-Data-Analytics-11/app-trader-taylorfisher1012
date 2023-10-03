e91f505f00b310148b58c86fca0db67711c65019
taylorfisher1012 <taylorfisher1012@gmail.com>
2023-10-03 12:53:51 -0400

    final_push

--- data/app_trader_script.sql
+++ data/app_trader_script.sql
@@ -1,93 +1,69 @@
---App trader project
+--App Trader
+--Your team has been hired by a new company called App Trader to help them explore and gain insights from apps that are made available through the Apple App Store and Android Play Store.   
+
+--App Trader is a broker that purchases the rights to apps from developers in order to market the apps and offer in-app purchases. The apps' developers retain all money from users purchasing the app from the relevant app store, and they retain half of the money made from in-app purchases. App Trader will be solely responsible for marketing any apps they purchase the rights to.
+
+--Unfortunately, the data for Apple App Store apps and the data for Android Play Store apps are located in separate tables with no referential integrity.
 --Deliverables
 	--a. Develop some general recommendations about the price range, genre, content rating, or any other app characteristics that the company should target.
-
 	--b. Develop a Top 10 List of the apps that App Trader should buy based on profitability/return on investment as the sole priority.
-
 	--c. Develop a Top 4 list of the apps that App Trader should buy that are profitable but that also are thematically appropriate for the upcoming Halloween themed campaign.
-
 	--d. Submit a report based on your findings. The report should include both of your lists of apps along with your analysis of their cost and potential profits. All analysis work must be done using PostgreSQL, however you may export query results to create charts in Excel for your report.
+--Top ten apps 
+WITH app_stats AS (SELECT name, 
+					p.rating, 
+					s.rating, 
+					ROUND(ROUND(((p.rating + s.rating)/2)*4)/4,2) AS avg_rating,
+					(p.review_count + s.review_count::INT)/2 AS avg_review_count,
+					MAX(p.price) AS app_store_price, 
+					MAX(s.price),
+					p.genres,
+					s.primary_genre AS game_genre,
+					(s.price+25000)::money AS app_sale_cost
+					FROM play_store_apps AS p INNER JOIN app_store_apps AS s USING(name)
+					WHERE s.price = 0 
+					GROUP BY name, 
+					p.rating,
+					s.rating, 
+					s.price,
+					p.genres,
+					s.primary_genre,
+					p.review_count,
+					s.review_count
+					ORDER BY avg_rating DESC)
+SELECT  name, 
+		game_genre,
+		app_store_price,
+		avg_rating,
+		avg_review_count,
+		app_sale_cost, 
+		(avg_rating * 2)+1 AS longevity_in_yrs,
+		((avg_rating * 2 + 1) *12 *1000)::money AS maintenance_cost, 
+		((avg_rating * 2 + 1) *12 *5000)::money AS app_revenue,
+		((avg_rating * 2 + 1) *12 *5000)::money - (((avg_rating * 2 + 1) *12 *1000)+2500)::money AS profit
+FROM app_stats
+WHERE avg_review_Count >= (SELECT AVG(avg_review_count)
+									FROM app_stats)
+ORDER BY avg_rating DESC;
 
-SELECT a.name, p.name,category,a.rating, p.rating, a.review_count AS app_review_count, p.review_count AS play_review_count ,a.content_rating, p.content_rating, p.install_count 
-FROM app_store_apps AS a FULL JOIN play_store_apps AS p USING(name)
-WHERE a.name IS NOT NULL AND p.name IS NOT NULL
-GROUP BY a.name, p.name,category,a.rating, p.rating, a.review_count, p.review_count ,a.content_rating, p.content_rating, p.install_count 
-ORDER BY a.name;
-
-SELECT a.name, p.name,category,a.rating, p.rating, a.review_count AS app_review_count, p.review_count AS play_review_count ,a.content_rating, p.content_rating, p.install_count 
-FROM app_store_apps AS a FULL JOIN play_store_apps AS p USING(name)
-WHERE a.name IS NOT NULL AND p.name IS NOT NULL
-GROUP BY a.name, p.name,category,a.rating, p.rating, a.review_count, p.review_count ,a.content_rating, p.content_rating, p.install_count 
-ORDER BY a.name;
-
-
-SELECT *
-FROM app_store_apps
-;
-
-SELECT a.price, p.price, a.rating, p.rating,a.content_rating 
-FROM app_store_apps AS a INNER JOIN play_store_apps AS p USING(name)
-ORDER BY a.rating DESC;
-
-SELECT a.name, ROUND(AVG((a.rating+p.rating)/2),2) AS tot_rating
-FROM app_store_apps AS a
-INNER JOIN play_store_apps AS p
-USING(name)
-GROUP BY a.name
-ORDER BY tot_rating DESC;
-
-
---Free apps from both the play and app store that excel in all categroies of reviews and and ratings 
-SELECT a.name,ROUND(AVG((a.rating + p.rating)/2),2) AS total_rating, MAX(a.price) AS price,
-MAX(p.review_count) AS play_store_reviews, MAX(a.review_count) AS app_store_reviews,
-p.genres,a.primary_genre, MAX(a.price) + 25000 AS price_to_buy_app
-FROM app_store_apps AS a 
-INNER JOIN play_store_apps AS p
-ON a.name = p.name
-WHERE p.review_count >= 
-			(SELECT AVG(review_count)
-			FROM play_store_apps) 
-		AND CAST(a.review_count AS int) >= 
-			(SELECT AVG(review_count::numeric)
-			FROM app_store_apps)
-GROUP BY a.name,p.genres,a.primary_genre
-ORDER BY total_rating DESC
-LIMIT 10
-
-SELECT name, ROUND((rating*24+1)/12) AS projected_life
-FROM play_store_apps;
-
---FINAL TABLE
-
-WITH data AS (SELECT a.name,ROUND(AVG(a.rating + p.rating)/2,2) AS total_rating,
-			 		ROUND((FLOOR(AVG(a.rating + p.rating)/2/.25)*.25),2) AS rounded_total_rating,
-					MAX(a.price) AS price,
-			 		MAX(p.review_count) AS play_store_reviews, 
-			 		MAX(a.review_count) AS app_store_reviews,
-			 		p.genres,a.primary_genre,MAX(a.price) + 25000 AS price_to_buy_app,
-			 		(((ROUND((FLOOR(AVG(a.rating + p.rating)/2/.25)*.25),2) *24)+12)) AS variable
-			 FROM app_store_apps AS a INNER JOIN play_store_apps AS p
-			 ON a.name = p.name
-			 WHERE p.review_count >= (SELECT AVG(review_count)
-									  FROM play_store_apps)  
-			  AND CAST(a.review_count AS int) >= (SELECT AVG(review_count::numeric)
-												  FROM app_store_apps)
-			 GROUP BY a.name,p.genres,a.primary_genre
-			 ORDER BY total_rating DESC
-			 LIMIT 10)
-SELECT name,total_rating,rounded_total_rating,price,play_store_reviews, app_store_reviews,
-			 genres,primary_genre, price_to_buy_app,
-			 ROUND(variable,2) AS longevity_in_months,
-			 ROUND((variable/12),2) AS longevity_in_years,
-			 ROUND(((variable+12)*5000),2)::money AS money_made_from_app,
-			 ROUND(((variable+12)*1000),2)::money AS app_maintenance,
-			 (ROUND(((variable+12)*5000),2)-ROUND(((variable+12)*1000),2))::money AS profit 
-FROM data;
+--Halloween Themed apps 
+WITH Halloween_apps AS (SELECT DISTINCT name, 
+ROUND((p.rating+ s.rating)/2,1) AS avg_rating,
+s.price AS price,
+(s.price+25000)::money AS cost_to_buy_app
+FROM play_store_apps AS p INNER JOIN app_store_apps AS s USING(name)
+WHERE name ILIKE '%zombie%'
+ORDER BY avg_rating DESC)
 
 
-
-
-
+SELECT name,
+avg_rating,
+price,
+(avg_rating * 2)+1 AS longevity_in_yrs,
+((avg_rating * 2 + 1) *12 *1000)::money AS maintenance_cost, 
+((avg_rating * 2 + 1) *12 *5000)::money AS app_revenue,
+((avg_rating * 2 + 1) *12 *5000)::money - (((avg_rating * 2 + 1) *12 *1000)+25000)::money AS profit
+FROM Halloween_apps;
 
 
 
